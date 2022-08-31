db4e29ef8e59d94baea64f657bb695ccc8245fe0
scholastica ngene <nnemms@gmail.com>
2022-08-31 10:13:09 -0700

    made some changes

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Passenger_counter
+# Built with
+
+HTML,CSS,JAVASCRIPT
+
+# What I learned
+
+Learnt the fundamentals of javascript and also DOM.
+
+# Acknowledgemt
+ Miss Jolade
--- script.js
+++ script.js
@@ -9,23 +9,28 @@
 let welcomeEl = document.getElementById("welcome-el")
 
 
-let name = "Dear user"
-let greeting = "Welcome back "
+let greeting = "Welcome"
 
-welcomeEl.innerText = greeting + name
+welcomeEl.innerText = greeting 
 
+welcomeEl.innerText += "👋"
+
+
+let saveEl = document.getElementById("save-el")
 
 let countEl = document.getElementById("count-el")
-
-console.log(countEl)
 
 let count = 0
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
 }
 
 function save() {
-    console.log(count)
-} 
+    let countString = count + " - "
+
+    saveEl.textContent += countString
+    countEl.textContent = 0
+    count = 0
+}
